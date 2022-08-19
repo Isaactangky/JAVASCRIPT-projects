b16548b1a5c1a3a5aabcfa1b8dffe6628e1544ee
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2022-08-19 15:38:43 +0800

    build to-do list app

---
+++ 5. To-do List/index.js
@@ -0,0 +1,40 @@
+const addTask = document.getElementById("add-task");
+const taskContainer = document.getElementById("task-container");
+const inputTask = document.querySelector("#input-task");
+
+// Event Listener for Add button
+addTask.addEventListener("click", function() {
+  let task = document.createElement("div");
+  task.classList.add("task");
+  let li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkBtn = document.createElement("button");
+  checkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  checkBtn.classList.add("checkTask");
+  task.appendChild(checkBtn);
+
+  let delBtn = document.createElement("button");
+  delBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  delBtn.classList.add("delTask");
+  task.appendChild(delBtn);
+
+  if (inputTask.value === ""){
+    alert("Please enter a task");
+  }else{
+    taskContainer.appendChild(task);
+  }
+  inputTask.value = "";
+
+  checkBtn.addEventListener("click", function(){
+    checkBtn.parentElement.style.textDecoration = "line-through";
+  
+  })
+  delBtn.addEventListener("click", function(e){
+    let target = e.target;
+    this.parentElement.remove();
+    
+  })
+  
+})
